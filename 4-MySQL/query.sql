8f7560251bceac95cd014189a15998d6b08f6172
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-09 09:03:59 -0300

    Reproduzir 19. INSERT, SELECT E WHERE

--- 4-MySQL/query.sql
+++ 4-MySQL/query.sql
@@ -4,17 +4,18 @@ CREATE DATABASE sistemadecadastro;
 SHOW DATABASES;
 USE sistemadecadastro;
 
--- No editor de código --
+-- No editor de código em seguida no console --
 CREATE TABLE usuarios(
   nome VARCHAR(50),
   email VARCHAR(100),
   idade INT
 );
 
--- No console do MySQL --
+-- No console direto do MySQL --
 SHOW TABLES;
 DESCRIBE usuarios;
 
+-- No editor de código em seguida no console --
 -- Inserindo Dados
 INSERT INTO usuarios(nome, email, idade) VALUES(
   "Victor Lima",
@@ -22,3 +23,32 @@ INSERT INTO usuarios(nome, email, idade) VALUES(
   8
 );
 
+INSERT INTO usuarios(nome, email, idade) VALUES(
+  "Luis Silva",
+  "email@teste@.com",
+  28
+);
+
+-- Inverter ordens não funciona mais no MySQL
+INSERT INTO usuarios(idade, email, nome)VALUES(
+  39
+  "email@teste@22.com",
+  "Lucas",
+);
+
+-- No console direto do MySQL --
+
+-- Inverter ordens não funciona mais no MySQL
+INSERT INTO usuarios(idade, email, nome)VALUES(
+  8
+  "maria@teste@22.com",
+  "Maria Clara",
+);
+
+SELECT * FROM usuarios;-- No console direto do MySQL --
+SELECT * FROM usuarios;
+
+-- WHERE - Listando com Filtros
+SELECT * FROM usuarios WHERE idade = 8;
+SELECT * FROM usuarios WHERE nome = "Lucas";
+SELECT * FROM usuarios WHERE idade >= 18;
