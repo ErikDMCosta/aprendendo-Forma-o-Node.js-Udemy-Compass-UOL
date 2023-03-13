63b0e58768c2d1c85d2e46d983620f0b3210cef9
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-13 11:17:57 -0300

    32. Adicionado card no formulário

--- 5-GuiaPerguntas/index.js
+++ 5-GuiaPerguntas/index.js
@@ -12,16 +12,27 @@ app.get("/:nome/:lang", (req, res) => {
   // var lang = "JavaScript";
   var nome = req.params.nome;
   var lang = req.params.lang;
-  var exibirMsg = true;
+  var exibirMsg = false;
   // res.render("principal/perfil");
-  res.render("/views/perguntar",{
+
+  var produtos = [
+    {nome: "Doritos",preco: 3.14},
+    {nome: "Coca-cola",preco:5},
+    {nome: "Leite",preco:1.45},
+    {nome: "Carne", preco:15},
+    {nome: "Redbull", preco: 6},
+    {nome: "Nescau", preco: 4}
+]
+
+  res.render("perguntar",{
     // nome: "ÉrikDMCosta",
     // lang: "JavaScript",
     nome: nome,
     lang: lang,
     empresa: "Guia do Programador",
     inscritos: 8040,
-    msg: exibirMsg
+    msg: exibirMsg,
+    produtos: produtos
   });
 });
 
