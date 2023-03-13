03a3a0a3b603b948f200732c38fadd4b6d2d1cc1
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-13 13:44:57 -0300

    Correção

--- 5-GuiaPerguntas/index.js
+++ 5-GuiaPerguntas/index.js
@@ -4,36 +4,41 @@ const app = express();
 // Estou dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs');
 
-app.get("/:nome/:lang", (req, res) => {
+// app.get("/:nome/:lang", (req, res) => {
+app.get("/", (req, res) => {
   // res.send("Bem vindo ao meu site!");
   // res.render("index");
   // res.render("home");
   // var nome = "ÉrikDMCosta";
   // var lang = "JavaScript";
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  var exibirMsg = false;
+  // var nome = req.params.nome;
+  // var lang = req.params.lang;
+  // var exibirMsg = false;
   // res.render("principal/perfil");
-
-  var produtos = [
-    {nome: "Doritos",preco: 3.14},
-    {nome: "Coca-cola",preco:5},
-    {nome: "Leite",preco:1.45},
-    {nome: "Carne", preco:15},
-    {nome: "Redbull", preco: 6},
-    {nome: "Nescau", preco: 4}
-]
-
-  res.render("perguntar",{
-    // nome: "ÉrikDMCosta",
-    // lang: "JavaScript",
-    nome: nome,
-    lang: lang,
-    empresa: "Guia do Programador",
-    inscritos: 8040,
-    msg: exibirMsg,
-    produtos: produtos
-  });
-});
+  
+  //   var produtos = [
+    //     {nome: "Doritos",preco: 3.14},
+    //     {nome: "Coca-cola",preco:5},
+    //     {nome: "Leite",preco:1.45},
+    //     {nome: "Carne", preco:15},
+    //     {nome: "Redbull", preco: 6},
+    //     {nome: "Nescau", preco: 4}
+    // ]
+    
+    // res.render("index",{
+      //   // nome: "ÉrikDMCosta",
+      //   // lang: "JavaScript",
+      //   nome: nome,
+      //   lang: lang,
+      //   empresa: "Guia do Programador",
+      //   inscritos: 8040,
+      //   msg: exibirMsg,
+      //   produtos: produtos
+      // });
+      res.render("index");
+    });
+    app.get("/perguntar", (req, res) => {
+      res.render("perguntar");
+    });
 
 app.listen(8080,()=>{console.log("App rodando!");})
