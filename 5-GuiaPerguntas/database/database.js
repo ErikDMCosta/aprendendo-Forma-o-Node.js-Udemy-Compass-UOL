15cb0fac30323895f8b1b89509246e38496b45f1
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-14 15:25:03 -0300

    42. Corrigindo erro de conexão do Sequelize

---
+++ 5-GuiaPerguntas/database/database.js
@@ -0,0 +1,9 @@
+const { ConnectionAcquireTimeoutError } = require('sequelize');
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('guiaperguntas', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+module.exports = connection;
