3f89bb7adc368bb3896b1693324bf1de703ed80f
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-16 16:31:39 -0300

    73. Base do projeto

--- 6-GuiaPress/index.js
+++ 6-GuiaPress/index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
+const connection = require("./database/database");
+const bodyParser = require("body-parser");
+
 
 // View engine
 app.set('view engine', 'ejs');
 
 // Static
 app.use(express.static('public'));
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Database
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com sucesso!")
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
 
 app.get("/", (req, res) => {
   res.send("Bem vindo ao meu site!");
