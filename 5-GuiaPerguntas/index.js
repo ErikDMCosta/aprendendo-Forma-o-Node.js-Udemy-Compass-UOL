f536ad203570abfa99a044ea6177ede9d2efb8c4
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-16 09:53:48 -0300

    55. Exibição de respostas e finalização

--- 5-GuiaPerguntas/index.js
+++ 5-GuiaPerguntas/index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const connection = require('./database/database');
-const Pergunta = require('./database/Pergunta');
-const Resposta = require('./database/Resposta');
-// Database
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 
+// Database
 connection
   .authenticate()
   .then(() => {
-    console.log('Conexão feita com o banco de dados!');
+    console.log("Conexão feita com o banco de dados!")
   })
   .catch((msgErro) => {
     console.log(msgErro);
-  });
+  })
 
-// Estou dizendo para o Express usar o EJS como view engine
+// Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // Body parser
@@ -24,9 +24,11 @@ app.use(bodyParser.json());
 // Rotas
 // app.get("/:nome/:lang", (req, res) => {
 app.get("/", (req, res) => {
-  Pergunta.findAll({ raw:true, order:[
-    ['id', 'DESC'] // ASC = Crescente || DESC = Decrescente 
-  ] }).then(perguntas => {
+  Pergunta.findAll({
+    raw: true, order: [
+      ['id', 'DESC'] // ASC = Crescente || DESC = Decrescente 
+    ]
+  }).then(perguntas => {
     //console.log(perguntas);
     res.render("index", {
       perguntas: perguntas
@@ -66,7 +68,7 @@ app.get("/", (req, res) => {
 });
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
-});
+})
 
 app.post("/salvarpergunta", (req, res) => {
   var titulo = req.body.titulo;
@@ -76,7 +78,7 @@ app.post("/salvarpergunta", (req, res) => {
 
   var titulo = req.body.titulo;
   var descricao = req.body.descricao;
-  
+
   Pergunta.create({
     // titulo: {
     //   type: Sequelize.STRING,
@@ -93,18 +95,39 @@ app.post("/salvarpergunta", (req, res) => {
   });
 });
 
-app.get("/pergunta/:id",(req, res) => {
+app.get("/pergunta/:id", (req, res) => {
   var id = req.params.id;
   Pergunta.findOne({
-    where: {id: id}
-  }).then(pergunta =>{
+    where: { id: id }
+  }).then(pergunta => {
     if (pergunta != undefined) { // Pergunta encontrada
-      res.render("pergunta", {
-        pergunta: pergunta
+      Resposta.findAll({
+        where: {
+          perguntaId: pergunta.id
+        },
+        order: [
+          ['id', 'DESC']
+        ]
+      }).then(respostas => {
+        res.render("pergunta", {
+          pergunta: pergunta,
+          respostas: respostas
+        });
       });
     } else { // Não encontrada
       res.redirect("/");
     }
+  });
+})
+
+app.post("/responder", (req, res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId
+  }).then(() => {
+    res.redirect("/pergunta/" + perguntaId); // Exemplo: res.redirect("/pergunta/3");
   });
 });
 
