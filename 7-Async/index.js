33914c39e7af1ced8dd6493ed777c0f0dcbde79d
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-20 17:09:15 -0300

    Aprendendo um pouco sobre Callbacks

--- 7-Async/index.js
+++ 7-Async/index.js
@@ -2,18 +2,26 @@
 //   console.log("Meu nome é Érik");
 // }, 5000); // Tempo em Milisegundos
 
-function enviarEmail(corpo, para) {
+function enviarEmail(corpo, para, callback) {
   setTimeout(() => {
     console.log(`
     Para: ${para}
     ----------------------------------
     ${corpo}
     ----------------------------------
+    De: Érik
     `)
+    callback();
   }, 8000);
 }
 
-console.log("Inicio do envio de e-mail!")
-enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com")
-console.log("Seu e-mail foi enviado, deve chegar em minutos")
-console.log("TUDO OK!")
+console.log("Inicio do envio de e-mail!");
+enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", () => {
+  //  console.log("OLÁ ISSO É UM CALLBACK!");
+  //  console.log("Ele acaba de ser executado!");
+  console.log("Seu e-mail foi enviado, deve chegar em minutos");
+  console.log("TUDO OK!");
+});
+
+// console.log("Seu e-mail foi enviado, deve chegar em minutos");
+// console.log("TUDO OK!");
