240559446a0e397b6fda95d612ea47960b1b355c
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-20 17:24:23 -0300

    Passa parâmetros dinamicamente para o callback

--- 7-Async/index.js
+++ 7-Async/index.js
@@ -11,17 +11,29 @@ function enviarEmail(corpo, para, callback) {
     ----------------------------------
     De: Érik
     `)
-    callback();
+    callback("OK",5,"8s","Érik"); // Possível ignorar paramêtros
   }, 8000);
 }
 
 console.log("Inicio do envio de e-mail!");
-enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", () => {
+enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", (status,amount,time, name) => { // Não ignorando o paramêtro agora
   //  console.log("OLÁ ISSO É UM CALLBACK!");
   //  console.log("Ele acaba de ser executado!");
-  console.log("Seu e-mail foi enviado, deve chegar em minutos");
+  // console.log("Seu e-mail foi enviado, deve chegar em minutos");
+  console.log(`
+  De: ${name}
+  Status: ${status}
+  ------------------
+  Contatos: ${amount}
+  ------------------
+  Tempo de Envio: ${time}
+  `);
   console.log("TUDO OK!");
 });
 
 // console.log("Seu e-mail foi enviado, deve chegar em minutos");
 // console.log("TUDO OK!");
+
+// app.get("/usuario", (req, res) => {
+
+// });
