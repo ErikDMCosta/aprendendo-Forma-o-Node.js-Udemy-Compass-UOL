d962202e2e0b221e762d550361d8aa8cc42d75a1
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-21 08:58:28 -0300

    Feito um tratamento de erros em callbacks

--- 7-Async/index.js
+++ 7-Async/index.js
@@ -4,31 +4,50 @@
 
 function enviarEmail(corpo, para, callback) {
   setTimeout(() => {
-    console.log(`
-    Para: ${para}
-    ----------------------------------
-    ${corpo}
-    ----------------------------------
-    De: Érik
-    `)
-    callback("OK",5,"8s","Érik"); // Possível ignorar paramêtros
+    // console.log(`
+    // Para: ${para}
+    // ----------------------------------
+    // ${corpo}
+    // ----------------------------------
+    // De: Érik
+    // `)
+
+    // -------- Lógica
+
+    // var deuErro = false;
+    var deuErro = true;
+    if (deuErro) {
+      callback(12,"O Envio do e-mail falhou!");
+    } else {
+      // callback("OK",5,"8s","Érik"); // Possível ignorar paramêtros
+      callback(12);
+    }
   }, 8000);
 }
 
 console.log("Inicio do envio de e-mail!");
-enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", (status,amount,time, name) => { // Não ignorando o paramêtro agora
+// enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", (status,amount,time, name) => { // Não ignorando o paramêtro agora
+
+enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", (time,erro) => {
+
+  if (erro == undefined) { // OK
+    console.log("TUDO OK!");
+  } else { // Opa, deu um erro!
+    console.log("Ocorreu um erro: " + erro);
+  }
+
   //  console.log("OLÁ ISSO É UM CALLBACK!");
   //  console.log("Ele acaba de ser executado!");
   // console.log("Seu e-mail foi enviado, deve chegar em minutos");
-  console.log(`
-  De: ${name}
-  Status: ${status}
-  ------------------
-  Contatos: ${amount}
-  ------------------
-  Tempo de Envio: ${time}
-  `);
-  console.log("TUDO OK!");
+  // console.log(`
+  // De: ${name}
+  // Status: ${status}
+  // ------------------
+  // Contatos: ${amount}
+  // ------------------
+  // Tempo de Envio: ${time}
+  // `);
+  // console.log("TUDO OK!");
 });
 
 // console.log("Seu e-mail foi enviado, deve chegar em minutos");
