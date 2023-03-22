f48817a46b470f4783bd2c026733194618ae4106
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-03-22 09:41:10 -0300

    Práticas de Promises aninhadas ou Promise Hell

--- 7-Async/index.js
+++ 7-Async/index.js
@@ -2,7 +2,8 @@
 //   console.log("Meu nome é Érik");
 // }, 5000); // Tempo em Milisegundos
 
-function enviarEmail(corpo, para, callback) {
+// function enviarEmail(corpo, para, callback) {
+  function enviarEmail(corpo, para, callback) {
   setTimeout(() => {
     // console.log(`
     // Para: ${para}
@@ -17,7 +18,7 @@ function enviarEmail(corpo, para, callback) {
     // var deuErro = false;
     var deuErro = true;
     if (deuErro) {
-      callback(12,"O Envio do e-mail falhou!");
+      callback(12, "O Envio do e-mail falhou!");
     } else {
       // callback("OK",5,"8s","Érik"); // Possível ignorar paramêtros
       callback(12);
@@ -28,12 +29,12 @@ function enviarEmail(corpo, para, callback) {
 console.log("Inicio do envio de e-mail!");
 // enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", (status,amount,time, name) => { // Não ignorando o paramêtro agora
 
-enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", (time,erro) => {
+enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", (time, erro) => {
 
   if (erro == undefined) { // OK
-    console.log("TUDO OK!");
+    // console.log("TUDO OK!");
   } else { // Opa, deu um erro!
-    console.log("Ocorreu um erro: " + erro);
+    // console.log("Ocorreu um erro: " + erro);
   }
 
   //  console.log("OLÁ ISSO É UM CALLBACK!");
@@ -57,23 +58,39 @@ enviarEmail("Oi, seja bem vindo ao Guia", "erik@udemy.com", (time,erro) => {
 
 // });
 
+function pegarId() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(5);
+    }, 1500);
+  });
+}
+
+function buscarEmailNoBanco() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("erik@udemy.com") // Teria Lógica de busca no Banco
+    }, 2000);
+  });
+}
+
 // enviarEmailPromise => "Eu prometo que..."
 
 function enviarEmailPromise(corpo, para) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       var deuError = false;
-      console.log("EMAIL ENVIADO!");
-      if(!deuError) {
-        resolve({time: 6, to: "victor@udemt.com"}); // Promessa OK!
+      // console.log("EMAIL ENVIADO!");
+      if (!deuError) {
+        resolve({ time: 6, to: "erik@udemy.com" }); // Promessa OK!
       } else {
         reject("Fila cheia"); // Foi mal, eu falhei :(
       }
-    },4000);
+    }, 4000);
   });
 }
 
-enviarEmailPromise("Olá Mundo","erik@udemy.com").then( ({ time, to }) => {
+enviarEmailPromise("Olá Mundo", "erik@udemy.com").then(({ time, to }) => {
   console.log(`
     Time: ${dados.time}
     ------------------
@@ -83,5 +100,17 @@ enviarEmailPromise("Olá Mundo","erik@udemy.com").then( ({ time, to }) => {
   // console.log("OPA, VOCÊ CONSEGUIU FAZER O QUE ME PROMETEU!");
   console.log(a);
 }).catch(() => {
-  console.log("QUE PENA, NÃO DEU :(");
+  // console.log("QUE PENA, NÃO DEU :(");
 });
+
+console.log("Inicio!");
+pegarId().then((id) => {
+  buscarEmailNoBanco(id).then((email) => {
+    enviarEmailPromise("Olá, como vai?", email).then(() => {
+      console.log("Email enviado, para ID: " + id);
+    }).catch(err => {
+      console.log(err);
+    });
+  })
+});
+console.log("FIM");
