26e3c07e722dfca9db3d0e9b09cb8306878ff51f
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-04-03 14:53:05 -0300

    Práticas da parte de Validando formulário

--- 10-Validacao/index.js
+++ 10-Validacao/index.js
@@ -21,9 +21,45 @@ app.use(session({
 
 app.use(flash());
 
-app.get("/",(req,res) => {
-  console.log("Está Rodando...");
-  res.send("Rodando...");
+app.get("/", (req, res) => {
+  // console.log("Está Rodando...");
+  // res.send("Rodando...");
+  res.render("index");
+});
+
+app.post("/form", (req, res) => {
+  var { email, nome, pontos } = req.body;
+
+  var emailError;
+  var pontosError;
+  var nomeError;
+
+  if (email == undefined || email == "") {
+    // err
+    emailError = "O e-mail não pode ser vazio";
+  }
+
+  if (pontos == undefined || pontos < 20) {
+    // err
+    pontosError = "Você não pode ter menos de 20 ponto";
+  }
+
+  if (nome == undefined || "") {
+    // err
+    nomeError = "O nome não pode ser vazio";
+  }
+  
+  if (nome.length < 4) {
+    nomeError = "O nome é muito pequeno";
+  }
+
+  if (emailError != undefined || pontosError != undefined || nomeError != undefined) {
+    // res.send("ESSE FORM É MUITO FEIO!");
+    res.redirect("/");
+  } else {
+    res.send("SHOW DE BOLA ESSE FORM!");
+  }
+  // res.send(email);
 });
 
 app.listen(5678, (res, req) => {
