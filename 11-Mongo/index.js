b2f9bacb2168d0bc493fed983af02a99db9add7b
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-04-04 17:50:23 -0300

    Criando Model, Busca Edita e Deletar dados

--- 11-Mongo/index.js
+++ 11-Mongo/index.js
@@ -1,3 +1,57 @@
 const mongoose = require("mongoose");
+// const articleModel = require("./article");
 
-mongoose.connect("mongodb+srv://localhost:27017/aprendendoMongo", {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect("mongodb://localhost:27017/aprendendoMongo", {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Configurando o mongoose de forma mais atual
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost:27017/aprendendoMongo").then(() => {
+  console.log('Mongo conectado...')
+}).catch((err) => {
+  console.log("Houve um erro ao se conectar ao MongoDB: " + err)
+});
+
+// Passando Artigos pelas Collections de forma antiga conforme o curso
+
+// const Article = mongoose.model("Article", articleModel);
+
+// const artigo = new Article({ title: "Desconhecido", author: "Não consigo ler nada", body: "Uma cassetada de Thank you" });
+
+articleModel.find({ '': '', 'body': 'React do Zero!' }).then(article => {
+  console.log(article);
+}).catch(err => {
+  console.log(err);
+});
+// artigo.save();
+
+// Model - User
+const aprendendoMongoSchema = mongoose.Schema({
+  title: {
+    type: String,
+    require: true
+  },
+  author: {
+    type: String,
+    require: true
+  },
+  body: {
+    type: String,
+    require: true
+  }
+});
+
+// Collection feito da formaa mais atual
+
+mongoose.model('aprendendoMongo', aprendendoMongoSchema);
+
+const user = mongoose.model('aprendendoMongo');
+
+new user({
+  title: "Desconhecido",
+  author: "Não consigo ler nada",
+  body: "Uma cassetada de Thank you"
+}).save().then(() => {
+  console.log("Usuário cadastrado com sucesso!")
+}).catch((err) => {
+  console.log("Houve um erro ao registrar o usuário: " + err)
+});
