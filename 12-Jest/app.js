c3abe1b0c95acca529b53db13d309515f76cc8fa
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2023-04-10 17:18:46 -0300

    Criando o Primeiro teste de TDD usando Jest

---
+++ 12-Jest/app.js
@@ -0,0 +1,15 @@
+module.exports = {
+  soma: function (a, b) {
+    return a + b;
+  },
+
+  // Teste aponta Falha
+  // mult: function (a, b) {
+  //   return a - b;
+  // }
+
+  //// Teste aponta Acerto
+  mult: function (a, b) {
+    return a * b;
+  }
+}
